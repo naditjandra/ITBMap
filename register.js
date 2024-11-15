fb05430f45485dc14d0ad82576f7b3eb9d2f08cb
naditjandra <francisaditjandra@gmail.com>
2024-11-15 18:35:52 +0700

    firebase integration

---
+++ register.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBfptLAYec8yv0JLxlmav4pNUFQPzWbGos",
+    authDomain: "destinyruler-7aa00.firebaseapp.com",
+    databaseURL: "https://destinyruler-7aa00-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "destinyruler-7aa00",
+    storageBucket: "destinyruler-7aa00.firebasestorage.app",
+    messagingSenderId: "519578709315",
+    appId: "1:519578709315:web:e0f30410ad622c16b613f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
